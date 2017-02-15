28a8d12fa25ffe670aef1d347b2e4565c706a4ff
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-15 22:12:58 +0300

    App config options added
    Company name added

--- appconfig.js
+++ appconfig.js
@@ -1,12 +1,15 @@
 var config = {};
 
+// Database connection
 config.pghost = 'localhost';
 config.pguser = 'postgres';
 config.pgpw = 'mazzofab';
 config.pgdatabase = 'gctl';
 config.pgport = '5432';
 
+// Company Details
 config.applogo = 'logotext.png'; // Logo for main application has to be stored in /public/images
 config.full_logo = 'logo.png'; // Logo for index page
+config.company_name = 'Greencity'; // Comany Name
 
 module.exports = config;
