d5e71708681906e92510fd8294552dcac4c74aad
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-17 12:46:09 +0300

    In insert db now we can exclude element from form updating DB
    Inset DB function will exclude all element that have a node class. It
    will not put them in the serialized for so the field will not be added
    in the query to the database

--- public/javascripts/insertdb_function.js
+++ public/javascripts/insertdb_function.js
@@ -2,7 +2,10 @@
 // - The function serializes the form which contains all the info such as table and fields to update
 // - The page has to be created with 2 hidden fields whcih are dbtable and returnid which are used in the insert query
 // - In order to reload the page after a sucessfull insert the hidden input app-page has to be set to the page we want to reload in workspace
-// After done clear all form Items
+// - After done clear all form Items
+
+// - When inserting we will exclude the inputs that that have a class nodb in them. This is usefull when getting input data from autocompletes
+// - The autocompletes create a hidden field woith the id of the item and this is what we use to insert into the DB, not the label text
 
 $(document).ready(function () {
 	$(document).on("click", "#btninsert", function (event) {
@@ -13,20 +16,19 @@ $(document).ready(function () {
 			}		
 		});
 		if($("#frmnewitem").valid() == true) {
+			console.log($("#frmnewitem").find(":input").not(".nodb").serializeArray())
 			$.ajax({
 				type: 'POST',
 				url: '/api/db/insert_db_field',
-				data: $("#frmnewitem").serializeArray(),
+				data: $("#frmnewitem").find(":input").not(".nodb").serializeArray(), // Exclude class nodb as this is used in autocompletes. We want the id and not the text
 				success: function (data) {
 					if (data == "OK") {
-						$("#succesfull_db").slideDown().delay(600).slideUp();
-						$("#workspace").load($("#apppage").val())
-						$("#frmnewitem").find("input[type=text], input[type=email], textarea").val("");
+						$("#succesfull_db").slideDown().delay(600).slideUp(); // Show success alert box
+						$("#workspace").load($("#apppage").val()) // Reload page in div to refresh data
+						$("#frmnewitem").find("input[type=text], input[type=email], textarea").val(""); // Clear the form after insert has been done
 						$("#frmnewitem").find("*").filter(":input:first").focus();
 					} else {
-						// Add the error message from backend to the alertbox
-						//$("#error_db").html($("#error_db").html() + JSON.stringify(data))
-						//$("#error_db").slideDown();
+						// - Display error in alert box
 						alert(JSON.stringify(data))
 					}
 				}, 
