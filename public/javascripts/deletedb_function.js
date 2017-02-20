edeb35697fc522a6d9675eb5b36a96ceb19b5c3b
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-20 15:59:45 +0300

    Separate main a set deletes
    Delete now checks wether we are deleting from main or details page

--- public/javascripts/deletedb_function.js
+++ public/javascripts/deletedb_function.js
@@ -2,12 +2,19 @@
 // - The button handler is included. You have to add a button with class="btndelete"
 // - In order to identify the line that has to be deleted we have to pass the id from the DB in the dbcolid attribute of the button
 // - The id value to be deleted is passed as attribute to the button like dbcolidval=<id value from DB>
-// - The table from where to delete is passed as value of <input type=hidden id=dbtable>
-
+// - The table from where to delete is passed as <input type=hidden id=dbatble value=<table name>>
+// - When deleting from detail tables an extra attricbute "isdetail=true" has to be passwd in order to identifythat we are
+// - updating a detail table and not a main table
 
 $(document).ready(function () {
 	$(document).on("click", ".btn-delete", function () {
 		var btn = $(this);
+		var table;
+		if ($(this).attr("isdetail") == "true") {
+			table = $("#dbtable-det").val()
+		} else {
+			table = $("#dbtable").val()	
+		}
 		if (confirm('ARE YOU SURE YOU WANT TO DELETE THIS ITEM?')) {
 			$.ajax({
 				type: 'POST',
@@ -16,15 +23,15 @@ $(document).ready(function () {
 					dbcolid: $(this).attr("dbcolid"),
 					dbcolidval: $(this).attr("dbcolidval"),
 					//table: btn.attr("dbtable")
-					table: $("#dbtable").val()
+					//table: $("#dbtable").val()
+					table: table,
 				},
 				success: function (data) {
 					if (data == "OK") {
 						$("#succesfull_db").slideDown().delay(1000).slideUp();
 						// remove row from table after succesfull DB Action
-						btn.closest("tr").remove();
+						btn.closest("tr").fadeOut();
 					} else {
-						//$("#error_db").slideDown().delay(1000).slideUp();
 						alert(JSON.stringify(data))
 					}
 				}, 
