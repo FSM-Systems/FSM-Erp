9ccc9259935600686101224d8a1bfee5878575c8
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-21 15:57:42 +0300

    Insert clears also number fields

--- public/javascripts/insertdb_function.js
+++ public/javascripts/insertdb_function.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 						if ($("#apppage").val() != "noreload") {
 							$("#workspace").load($("#apppage").val()) // Reload page in div to refresh data
 						}
-						$("#frmnewitem").find("input[type=text], input[type=email], textarea").val(""); // Clear the form after insert has been done
+						$("#frmnewitem").find("input[type=text], input[type=email], input[type=number], textarea").val(""); // Clear the form after insert has been done
 						$("#frmnewitem").find("*").filter(":input:first").focus();
 					} else {
 						// - Display error in alert box
