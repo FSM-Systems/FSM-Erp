4a75d4f5411653fc5fdb90f37aa71ae9dad0bc09
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-21 12:52:05 +0300

    Changed favicon and program name

--- appconfig.js
+++ appconfig.js
@@ -10,7 +10,7 @@ config.pgport = '5432';
 // Company Details
 config.applogo = 'logotext.png'; // Logo for main application has to be stored in /public/images
 config.full_logo = 'logo.png'; // Logo for index page
-config.company_name = 'Greencity'; // Comany Name
+config.company_name = 'FSM-Erp'; // Comany Name
 
 // Session settings
 config.session_time = 240 * 60000;
