8993bda6e0513122cfe35784354aa06a882f27e0
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-25 14:42:25 +0300

    Changed upload attributes
    Now we only pass upload directory as filename comes from the file
    object. A timestamp is appended to the file name so we do not overwrite
    existing files with same name.

--- routes/uploadapi.js
+++ routes/uploadapi.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
+var excel_parser = require('../excel_parser.js')
 
 // Upload a file GENERIC
-router.post('/file_upload/name/:ufilename/uploaddir/:uploaddir', function (req, res) {
+router.post('/file_upload/uploaddir/:uploaddir', function (req, res) {
 	if (!req.files) 
 		return res.status(400).send('No files were uploaded.');	
 	
 	let file = req.files.file
 	
 	// Move the file to uplods directory
-	file.mv('/tmp/' + req.params.ufilename, function (err) {
+	file.mv('/tmp/' + new Date().getTime() + '_' + file.name, function (err) {
 		if (err)
       		return res.status(500).send(err);	
 	})	
 })
 
 // Upload excel file for warehouse
-router.post('/excel_upload/name/:ufilename/uploaddir/:uploaddir', function (req, res) {
+router.post('/excel_upload/uploaddir/:uploaddir', function (req, res) {
 	if (!req.files) 
 		return res.status(400).send('No files were uploaded.');	
 	
@@ -26,11 +27,13 @@ router.post('/excel_upload/name/:ufilename/uploaddir/:uploaddir', function (req,
 	var uploaddir = __dirname.replace("routes",req.params.uploaddir + "/").toString(); // Create upload path based on name and upload directory
 	
 	// Move the file to selected directory
-	file.mv(uploaddir + req.params.ufilename, function (err) {
+	// Append timestamp so we do not overwrite any files
+	file.mv(uploaddir + new Date().getTime() + '_' + file.name, function (err) {
 		if (err)
       		return res.status(500).send(err);	
 	})	
 	//console.log(req.files.file);
+	excel_parser.parse_xlsx(file.name)
 })
 
 module.exports = router;
