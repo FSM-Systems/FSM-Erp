0ff5f133f50e03a124136726c59d5b5e25d46408
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-26 12:27:39 +0300

    Upload api improved
    Added parse excel functions for excel bulk upload in warehouses

--- routes/uploadapi.js
+++ routes/uploadapi.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
-var excel_parser = require('../excel_parser.js')
+var fsm = require('../fsm_functions.js')
 
 // Upload a file GENERIC
 router.post('/file_upload/uploaddir/:uploaddir', function (req, res) {
@@ -11,14 +11,14 @@ router.post('/file_upload/uploaddir/:uploaddir', function (req, res) {
 	let file = req.files.file
 	
 	// Move the file to uplods directory
-	file.mv('/tmp/' + new Date().getTime() + '_' + file.name, function (err) {
+	file.mv(req.params.uploaddir + '/' + new Date().getTime() + '_' + file.name, function (err) {
 		if (err)
       		return res.status(500).send(err);	
 	})	
 })
 
 // Upload excel file for warehouse
-router.post('/excel_upload/uploaddir/:uploaddir', function (req, res) {
+router.post('/excel_upload/uploaddir/:uploaddir', function (req, res, next) {
 	if (!req.files) 
 		return res.status(400).send('No files were uploaded.');	
 	
@@ -28,12 +28,16 @@ router.post('/excel_upload/uploaddir/:uploaddir', function (req, res) {
 	
 	// Move the file to selected directory
 	// Append timestamp so we do not overwrite any files
-	file.mv(uploaddir + new Date().getTime() + '_' + file.name, function (err) {
+	var newfile = uploaddir + new Date().getTime() + '_' + file.name;
+	file.mv(newfile, function (err) {
 		if (err)
-      		return res.status(500).send(err);	
+      		return res.status(500).send(err);
+		// Parse excel file
+		if (fsm.parse_excel(newfile, req.session.user_id) == true) {
+			// redirecto to confirmation page
+			res.send('OK');
+		}
 	})	
-	//console.log(req.files.file);
-	excel_parser.parse_xlsx(file.name)
 })
 
 module.exports = router;
