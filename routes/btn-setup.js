cdf8d7e1420e68f5d0dda42ab8ce09e25b8b55e7
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-02-28 15:17:47 +0300

    Print goods issue note

--- routes/btn-setup.js
+++ routes/btn-setup.js
@@ -6,7 +6,7 @@ var config = require('../appconfig');
 // User permissions
 router.get('/user_permissions/param/:param', function (req, res, next) {
 	// Get all perms available. Basically its all the items in the menu table
-	db.query('select mid, mdescription from menu order by mgroup, morder', function (err, result) {
+	db.query('select mid, mdescription, mgdescription from menu left join menu_groups on mgroup=mgid order by mgroup, morder', function (err, result) {
 		db.query('select lpermissions, ldescription from login where lid=$1', [req.params.param], function (err, result2) {
 			// For user permission we are going to pass the lpermissions column which is actually a comma separated field in the DB that can be iterated through
 			res.render('btn-setup/user_permissions', { 
@@ -25,7 +25,7 @@ router.get('/add_to_goods_received_note/param/:param', function (req, res, next)
 	db.query('select * from vw_goods_issued_notes_details where gind_ginid=$1', [req.params.param], function (err, result) {
 		res.render('btn-setup/add_to_goods_received_note.pug', {
 			title: 'Add items to Goods Received Notes',	
-			ginid: req.params.param,
+			gin: req.params.param,
 			items: result.rows,
 		})
 	})
