3722fd37b68aabcfdbd619c6a12cf50a1133fef3
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-03-01 15:35:15 +0300

    Added generic readonly and copy paste options to user setup

--- routes/btn-setup.js
+++ routes/btn-setup.js
@@ -7,15 +7,15 @@ var config = require('../appconfig');
 router.get('/user_permissions/param/:param', function (req, res, next) {
 	// Get all perms available. Basically its all the items in the menu table
 	db.query('select mid, mdescription, mgdescription from menu left join menu_groups on mgroup=mgid order by mgroup, morder', function (err, result) {
-		db.query('select lpermissions, ldescription from login where lid=$1', [req.params.param], function (err, result2) {
+		db.query('select lpermissions, ldescription, lcopypaste, lreadonly from login where lid=$1', [req.params.param], function (err, result2) {
 			// For user permission we are going to pass the lpermissions column which is actually a comma separated field in the DB that can be iterated through
 			res.render('btn-setup/user_permissions', { 
-				menu: result.rows,  // All permissions available
-				userperms: result2.rows[0].lpermissions,  // Permissions of this user
+				menu: result.rows,  // All menu items available
+				mdata: result2.rows,  // Permissions of this user
 				title: 'Setup User Permissions',  // Page title
-				username: result2.rows[0].ldescription, // Username of the selected user,
-				userid: req.params.param // login table ID of the selected user
-			}); 
+				user_id: req.params.param,
+				user_description: result2.rows[0].ldescription
+			});
 		})	
 	})
 })
